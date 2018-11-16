502d33cdabd0612eaa47f47fb5c0da0283040f4e
Phil Mass <philrmass@gmail.com>
2018-11-16 10:02:52 -0800

    pass multiple letter test, fix gemfile typo'

--- lib/word_compare.rb
+++ lib/word_compare.rb
@@ -20,8 +20,14 @@ class WordCompare
     other_letters = other_word.split('')
     other_letters.each do |other_letter|
       if letter_hash.include?(other_letter)
-        letter_hash.delete(other_letter)
-        p "  DELETE #{other_letter}  hash=#{letter_hash}"
+        letter_count = letter_hash.fetch(other_letter)
+        if letter_count <= 1
+          letter_hash.delete(other_letter)
+          p "  DELETE #{other_letter}  hash=#{letter_hash}"
+        else
+          letter_hash[other_letter] = letter_count - 1
+          p "  DEC #{other_letter}  cnt=#{letter_count - 1}  hash=#{letter_hash}"
+        end
       else
         p "  MISSING #{other_letter}  hash=#{letter_hash}"
         is_anagram = false
